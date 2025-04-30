f03618ff48f036533826353a650b0bb69d872085
WINEZERO <kissloo@foxmail.com>
2025-04-30 16:55:40 +0800

    update

--- my-plugins/plugins-frame-frontend/vue.js.rb
+++ my-plugins/plugins-frame-frontend/vue.js.rb
@@ -6,6 +6,11 @@ authors [
 
 version "0.1"
 matches [
+{:offset=>1, :search=>'body', :regexp=>/vue[.-]([\d.]*\d)[^.]*\.js/},
+{:regexp=>/(?:.([\d.]+))?.vue(?:\.min)?\.js/,:offset=>1, :search=>'body'},
+{:regexp=>/<[^>]+\sdata-v(?:ue)?-/},
+{:text=>'vue.js'},
 {:mmh3=>'-1252041730'},
+{:mmh3=>'-1237480336'},
 ]
 end
