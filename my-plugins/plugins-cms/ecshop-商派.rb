28b18bcfa0cab309c367241dfb4cd02ca04734e9
WINEZERO <kissloo@foxmail.com>
2025-05-19 16:36:14 +0800

    fix ecshop

--- my-plugins/plugins-cms/ecshop-商派.rb
+++ my-plugins/plugins-cms/ecshop-商派.rb
@@ -1,11 +1,15 @@
 Plugin.define do
 name "ecshop-商派"
 authors [
-"winezero",
-
+	"winezero"
 ]
 version "0.1"
+description "商派 Ecshop"
+website "http://www.68ecshop.com"
 matches [
-{:search=>"all", :text=>'Ecshop(商派)'},
+{:url=>'images/login_dl.jpg', :mmh3=>'1659467063'},
+{:search=>"body", :text=>'<a href="get_password.php?act=forget_pwd">您忘记了密码吗?</a></td>'},
+{:search=>"body", :text=>'background:url(images/login_bg.png)'},
+{:search=>"body", :text=>'background:url(images/login_dl.jpg)'},
 ]
 end
